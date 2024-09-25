fdcd46dbfd35770ea2a21ecdb3d68ddcff266ad5
Teklemya <Teklemya@mail.uc.edu>
2024-09-24 21:03:17 -0400

    inital commit

---
+++ samosa selector/src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import "./App.css";
+import samosa from "../src/assets/samosa.jpeg";
+
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  const [multiplier] = useState(1);
+
+  const updateCount = () => setCount(count + multiplier);
+
+  return (
+    <div className="App">
+      <div className="header">
+        <h1>Samosa Selector</h1>
+        <h2>Count: {count}</h2>
+        <img
+          className="samosa"
+          onClick={updateCount}
+          src={samosa}
+          alt="samosa image"
+        />
+      </div>
+      <div className="container">
+        <div className="upgrade">
+          <h3>Double Stuffed 👯‍♀️	</h3>
+          <p>2x per click</p>
+          <button>10 samosas</button>
+        </div>
+        <div className="upgrade">
+          <h3>Party Pack 🎉</h3>
+          <p>5x per click</p>
+          <button>100 samosas</button>
+        </div>
+        <div className="upgrade">
+          <h3>Full Feast 👩🏽‍🍳</h3>
+          <p>10x per click</p>
+          <button>1000 samosas</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
